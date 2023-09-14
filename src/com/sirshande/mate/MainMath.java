2616b39e2a91e1a5577c8515ef4bad831ca5a205
Ghetto97 <fernandez.shande@gmail.com>
2023-09-14 03:20:45 -0500

    Retoques

--- src/com/sirshande/mate/MainMath.java
+++ src/com/sirshande/mate/MainMath.java
@@ -13,30 +13,30 @@ import javax.swing.*;
 public class MainMath {
     public static void main(String[] args) {
 
-            double a = Double.parseDouble(JOptionPane.showInputDialog("Introduce el coeficiente 'a':"));
-            double b = Double.parseDouble(JOptionPane.showInputDialog("Introduce el coeficiente 'b':"));
-            double c = Double.parseDouble(JOptionPane.showInputDialog("Introduce el coeficiente 'c':"));
+            double a = Double.parseDouble(JOptionPane.showInputDialog("Ingrese el número, coeficiente 'a':", "10"));
+            double b = Double.parseDouble(JOptionPane.showInputDialog("Ingrese el número, coeficiente 'b':", "11"));
+            double c = Double.parseDouble(JOptionPane.showInputDialog("Ingrese el número, coeficiente 'c':", "12"));
 
             double[] coeficientes = { a, b, c };
             Ecuacion ecuacion = new Ecuacion(coeficientes);
 
-            double x = Double.parseDouble(JOptionPane.showInputDialog("Introduce el valor de x:"));
+            double x = Double.parseDouble(JOptionPane.showInputDialog("Ingrese el valor de x:", "8"));
             double resultado = ecuacion.resolver(x);
             JOptionPane.showMessageDialog(null, "El resultado de la ecuación para x=" + x + " es: " + resultado, "Resultado", JOptionPane.INFORMATION_MESSAGE);
 
-            double base = Double.parseDouble(JOptionPane.showInputDialog("Introduce el valor de la base:"));
-            double exponente = Double.parseDouble(JOptionPane.showInputDialog("Introduce el valor del exponente:"));
+            double base = Double.parseDouble(JOptionPane.showInputDialog("Ingrese el valor de la base:", "9"));
+            double exponente = Double.parseDouble(JOptionPane.showInputDialog("Ingrese el valor del exponente:", "2"));
 
             double potencia = Math.pow(base, exponente);
             JOptionPane.showMessageDialog(null, base + " elevado a la " + exponente + " es igual a " + potencia, "Potencia", JOptionPane.INFORMATION_MESSAGE);
 
-            double valor = Double.parseDouble(JOptionPane.showInputDialog("Introduce el valor para calcular la raíz cuadrada:"));
+            double valor = Double.parseDouble(JOptionPane.showInputDialog("Ingrese el valor para calcular la raíz cuadrada:", "36"));
 
             double raíz = Math.sqrt(valor);
             JOptionPane.showMessageDialog(null, "La raíz cuadrada de " + valor + " es igual a " + raíz, "Raíz Cuadrada", JOptionPane.INFORMATION_MESSAGE);
 
             try {
-                double valorCalc = Double.parseDouble(JOptionPane.showInputDialog("Introduce un valor para calcular la raíz cuadrada mejorada:"));
+                double valorCalc = Double.parseDouble(JOptionPane.showInputDialog("Ingrese un valor para calcular la raíz cuadrada mejorada:", "25"));
 
                 double raízMejorada = Math.sqrtMejorado(valorCalc);
                 JOptionPane.showMessageDialog(null, "La raíz cuadrada mejorada de " + valorCalc + " es igual a " + raízMejorada, "Raíz Cuadrada Mejorada", JOptionPane.INFORMATION_MESSAGE);
